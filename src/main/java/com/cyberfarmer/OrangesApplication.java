e6edd3289c9499e91eeeb4502784cd73108a6925
nask137 <2837509831@qq.com>
2024-03-28 09:17:36 +0800

    项目初始化,添加maven坐标,工具包以及初始包

---
+++ src/main/java/com/cyberfarmer/OrangesApplication.java
@@ -0,0 +1,13 @@
+package com.cyberfarmer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrangesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrangesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cyberfarmer/OrangesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyberfarmer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrangesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
